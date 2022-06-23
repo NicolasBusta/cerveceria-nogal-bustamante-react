f50513e61e6d4dc6df570f2db5511a6111499c0b
NicolasBusta <nicolasbustamanteb1@gmail.com>
2022-06-23 13:10:39 -0300

    bords

--- src/ui/components/Item.js
+++ src/ui/components/Item.js
@@ -2,8 +2,8 @@ import {Link} from "react-router-dom"
 
 const Item = ({item}) => {
   return (
-    <article className="item">
-      <h2>{item.title}</h2>
+    <article className="item border border-dark">
+      <h4>{item.title}</h4>
       <img src={item.image} alt="thumbnail" />
       <p>Precio : ${item.price}</p>
       <Link type="button" class="btn btn-info" to={`/item/${item.id}`}>ver detalle</Link>
